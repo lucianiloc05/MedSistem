da2fb2f92b6423a5ae55e64e5700d3f32301d0ec
RKorbuly <rkorbulyy@gmail.com>
2026-03-29 20:47:37 +0300

    Update Identificator.java

--- src/Identificator.java
+++ src/Identificator.java
@@ -3,14 +3,17 @@ public class Identificator {
     private Long id;
     private boolean consentGranted = false;
     private int nivelAcces = -1; //-1 privat, 0 doctor, 1 public
-
+    private boolean activat = false;
     public void activare(){
+        activat = !activat;
+    }
+    public void grantConsent(){
         consentGranted = !consentGranted;
     }
-
-
+    
     public int getNivel() { return nivelAcces; }
     public void setNivel(int niv) { nivelAcces = niv; }
+    public boolean getActivare() { return activat; }
 
     Identificator(Long id){
         this.id = id;
