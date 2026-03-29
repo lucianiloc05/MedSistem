4fdb8ce63ddbfd71848012e5719ab41ffdf30fc9
RKorbuly <rkorbulyy@gmail.com>
2026-03-29 20:47:03 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,22 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-import static java.lang.System.exit;
-
 public class Main {
     public static void main(String[] args) {
+        // Simulare caz de utilizare Activare Identificator
+        
+        MedSistem sistem = new MedSistem();
+        
+        Cetatean cetatean = new Cetatean("Popescu Ion", "1900101123456");
 
-        //Creare de date 'dummy'
-        MedSistem Sis = DummyData.FillData();
-
-        BufferedReader cin = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("Programul ruleaza. Sunteti Doctor sau Pacient?");
-        System.out.println("Introduceti 'Doctor' / 'Pacient': ");
-
-        try{
-            String option = cin.readLine();
-            String option1;
-
-            if(option.equals("Doctor")){
-                System.out.println("Optiuni: ");
-                System.out.println("1. Visualizare Pacienti: ");
-                System.out.println("2. Selectare Pacient: ");
-                System.out.println("3. Oprire: ");
-
-                while(!option.equals("3")) {
-
-
-                    option1 = cin.readLine();
-
-                    if (option1.equals("1")) {
-                        Sis.afisarePacienti();
-                    }
-
-                    if (option1.equals("2")){
-                        String option2;
-                        System.out.println("Introdu numele unui pacient: ");
-
-                        while(true) {
-                            option2 = cin.readLine();
-                        }
-                    }
-                }
-
-            }
+        System.out.println("Sa creat cetateanul");
+        sistem.registerCetatean(cetatean);
+        sistem.generareID(cetatean);
+        
+        sistem.activareID(cetatean.getId());
+        
+        if(cetatean.getId().getActivare() == true){
+            System.out.println("Sa activat cetateanul");
         }
-        catch (Exception E){
-            System.out.println(E);
+        else {
+            System.out.println("Nu sa activat cetateanul");
         }
-
-
     }
 }
